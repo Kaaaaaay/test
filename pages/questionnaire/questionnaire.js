0b5314f74749c758ac70ec75570ebe3e260af019
Liu, Si Kai (Northern Lights) <SKLiu@nltechdev.com>
2019-02-18 15:45:37 +0800

    Initial Commit

---
+++ pages/questionnaire/questionnaire.js
@@ -0,0 +1,90 @@
+// pages/questionnaire/questionnaire.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  formSubmit(e) {
+    wx.request({
+      url: 'http://localhost:12185/api/Questionnaire',
+      method: 'post',
+      data: {
+        UserName: 'test',
+        Question1: 'q1',
+        Question2: 'q2',
+        Question3: 'q3',
+        Chance1: 'c1',
+        Chance2: 'c2',
+        Chance3: 'c3'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset() {
+    console.log('form发生了reset事件')
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
